22ccd52bc3cd527dbc471408bbda11fc629636f5
JuanAntonaccio <64330819+JuanAntonaccio@users.noreply.github.com>
2022-03-07 07:30:24 -0300

    cambios funcionando timer

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let actual = null // me dice la tarea actual que se esta ejecutando
 const bAdd = document.querySelector("#bAdd")
 const itTask= document.querySelector("#itTask")
 const form = document.querySelector("#form")
+const taskName = document.querySelector('#time #taskName')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault() // anulamos su funcionamiento nativo
@@ -46,4 +47,45 @@ function renderTask(){
     })
     const taskContainer = document.querySelector("#tasks")
     taskContainer.innerHTML = html.join('')
+
+    const startButtons = document.querySelectorAll('.task .start-button') // devuelve una coleccion
+    startButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if(!timer){
+                const id = button.getAttribute('data-id')
+                startButtonHandler(id)
+                button.textContent = 'En Progreso ....'
+            }
+        })
+    })
+}
+
+function startButtonHandler(id){
+    time = 25 * 60
+    actual = id
+    const taskIndex = tareas.findIndex(task => task.id = id)
+    
+    taskName.textContent = tareas[taskIndex].title
+    timer = setInterval(() =>{
+        timeHandler(id)
+    },1000)// 1000 ms = 1 seg
+
+}
+
+function timeHandler(id){
+    time--
+    renderTime()
+    if (time==0){
+        clearInterval(timer)
+        actual=null // porque ya terminamos la tarea
+        taskName.textContent=''
+        renderTime()
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector("#time #value")
+    const minutes = parseInt(time / 60)
+    const seconds = parseInt(time % 60)
+    timeDiv.textContent = `${minutes <10 ? '0' :''}${minutes}:${seconds<s10 ? '0':''}${seconds}`
 }
